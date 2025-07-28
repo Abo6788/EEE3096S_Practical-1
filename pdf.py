b50eb76c8cc08b78f6b2ab33be31af6499c6f39a
EEE3096S-UCT <eee3096s.2025@gmail.com>
2025-07-28 10:31:04 +0200

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -2,10 +2,14 @@ from fpdf import FPDF
 
 # Load the contents of the main.c file
 <<<<<<< HEAD
+<<<<<<< HEAD
 with open("core/src/main.c", "r") as file:
 =======
 with open("/mnt/data/main.c", "r") as file:
 >>>>>>> ab3183e (Added Prac1 Files)
+=======
+with open("core/src/main.c", "r") as file:
+>>>>>>> 0f67727 (Update pdf.py)
     c_code = file.read()
 
 # Initialize PDF
@@ -20,10 +24,14 @@ for line in c_code.splitlines():
 
 # Save the PDF
 <<<<<<< HEAD
+<<<<<<< HEAD
 output_path = "main_c_code.pdf"
 =======
 output_path = "/mnt/data/main_c_code.pdf"
 >>>>>>> ab3183e (Added Prac1 Files)
+=======
+output_path = "main_c_code.pdf"
+>>>>>>> 0f67727 (Update pdf.py)
 pdf.output(output_path)
 
 
